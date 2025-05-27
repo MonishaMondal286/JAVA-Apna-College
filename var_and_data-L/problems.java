d8f15599c1097ba4f656baad02ea0ebd943c1db6
Monisha Mondal 286 <mondalmonisha286@gmail.com>
2025-05-27 19:40:32 +0530

    done with var and datatypes

--- var_and_data-L/problems.java
+++ var_and_data-L/problems.java
@@ -13,5 +13,33 @@ public class problems {
    }
 }
 */
-
+/* 
 // problem - 2
+import java.util.*;
+public class problems{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int area = s*s;
+        System.out.println(area);
+    }
+}
+
+*/
+
+// problem - 3
+import java.util.*;
+public class problems{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float pen = sc.nextFloat();
+        float pencil = sc.nextFloat();
+        float eraser = sc.nextFloat();
+        float cost = (pen+pencil+eraser)*0.18f;
+        System.out.println(cost);
+    
+    }
+}
+
+
+
