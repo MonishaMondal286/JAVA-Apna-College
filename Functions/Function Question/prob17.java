bff5229192b183658a1b8f9fa00eb5f75d701224
Monisha Mondal 286 <mondalmonisha286@gmail.com>
2025-06-18 17:58:07 +0530

    functions

--- Functions/Function Question/prob17.java
+++ Functions/Function Question/prob17.java
@@ -1,5 +1,15 @@
 // 17. **Write a method that converts Celsius to Fahrenheit.**
 
 public class prob17 {
+    public static double temperature(double temp){
+        double Faren = (9.0/5)*temp+32;
+        return Faren;
+    }
     
+    
+
+    public static void main(String args[]){
+       System.out.println(temperature(32));
+       
+    }
 }
