6aa9276d0f4631f1ed89f62374290513bf62ad85
Monisha Mondal 286 <mondalmonisha286@gmail.com>
2025-07-09 00:08:37 +0530

    updated

--- Sorting/Sorting Concepts/SelectSort.java
+++ Sorting/Sorting Concepts/SelectSort.java
@@ -1,9 +1,40 @@
+
+// public class SelectSort{
+//     public static void SelSort(int arr[]){
+//         for(int i=0;i<arr.length;i++){
+//             int minpos = i;
+//             for(int j=i+1;j<arr.length;j++){
+//                 if(arr[minpos]>arr[j]){
+//                     minpos = j;
+//                 }
+            
+//             }
+//                 //swap 
+//             int temp = arr[minpos];
+//             arr[minpos] = arr[i];
+//             arr[i] = temp;
+//         }
+       
+//     }
+
+//     public static void main(String args[]){
+//         int arr[] = {3,2,1,4,5};
+//         SelSort(arr);
+//         System.out.println("array after sorting : ");
+//         for(int i=0;i<arr.length;i++){
+//             System.out.print(arr[i]+ " ");
+//         }
+//     }
+// }
+
+
+//decreasing order 
 public class SelectSort{
     public static void SelSort(int arr[]){
         for(int i=0;i<arr.length;i++){
             int minpos = i;
             for(int j=i+1;j<arr.length;j++){
-                if(arr[minpos]>arr[j]){
+                if(arr[minpos]<arr[j]){
                     minpos = j;
                 }
             
@@ -21,7 +52,7 @@ public class SelectSort{
         SelSort(arr);
         System.out.println("array after sorting : ");
         for(int i=0;i<arr.length;i++){
-            System.out.print(i+ " ");
+            System.out.print(arr[i]+ " ");
         }
     }
 }
