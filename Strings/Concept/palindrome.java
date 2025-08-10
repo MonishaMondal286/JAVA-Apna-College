c0252f6e9d93778b56a288b4165bcb95d5413d6d
Monisha Mondal 286 <mondalmonisha286@gmail.com>
2025-08-10 11:55:29 +0530

    palindrome

---
+++ Strings/Concept/palindrome.java
@@ -0,0 +1,18 @@
+public class palindrome {
+    public static boolean CheckPalindrome(String str){
+        for(int i=0;i<str.length();i++){
+            int n = str.length();
+            if((str.charAt(i)) != (str.charAt(n-i-1))){
+                //not  a palindrome 
+                return false;
+            }
+        }
+        return true;
+
+        }
+    public static void main(String args[]){
+            String word = "racecar";
+
+            System.out.println(CheckPalindrome(word));
+        }
+}
