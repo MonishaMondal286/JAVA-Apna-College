7573009d5bc890cb5435eb538f0919c564afc476
Monisha Mondal 286 <mondalmonisha286@gmail.com>
2025-08-18 19:34:02 +0530

    msg

---
+++ Strings/Concept/basic.java
@@ -0,0 +1,33 @@
+public class basic {
+    public static void main(String args[]){
+        // String a = "Test";
+        // String b = new String("Test");
+        // System.out.println(a==b);
+        // System.out.println(a.equals(b));
+        
+        //reverse
+        // String word = "java";
+        // for(int i=word.length()-1;i>=0;i--){
+        //    System.out.print(word.charAt(i));
+        // }
+
+        // String s1 = "hi";
+        // String s2 = "hello";
+        // System.out.println(s1.concat(s2));
+
+
+        //substring 
+        String s = "Java";
+        for(int i=0;i<=3;i++){
+            for(int j=i+1;j<=4;j++){
+                System.out.print(s.substring(i,j)+" ");
+            }
+        }
+        
+        
+        
+
+        
+    }
+}
+     
